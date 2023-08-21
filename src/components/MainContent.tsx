77ceb3566d146605354aff150ef9ad5b60fce151
Emilia <emiliamassings@gmail.com>
2023-08-21 20:29:31 +0200

    💄 Added img to startpage, font for headings, tweaked colors

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -9,8 +9,7 @@ export function MainContent() {
                 <Outlet></Outlet>
             </main>
             <footer>
-                <a href="https://www.freepik.com/free-photo/abstract-green-grunge-background_4037904.htm#query=dark%20green%20background&position=0&from_view=keyword&track=ais">Backround image by denamorado </a>
-                <span>on Freepik</span>
+                <span>Created in august 2023</span>
             </footer>
         </>  
     );
--- src/components/StartPage.tsx
+++ src/components/StartPage.tsx
@@ -1,34 +1,42 @@
+import startpage from "../assets/startpage.png"
+
 export function StartPage() {
     localStorage.removeItem('animalInfo');
 
     return (
         <>
-            <h1>Nordiska Djurriket</h1>
-
             <section className="informationContainer">
-                <p>
-                    Välkommen till vårt fantastiska zooäventyr! Upplev magin med en värld av fascinerande djur från hela jorden samlade på ett ställe. 
-                    Vår djurpark är en plats där lärande och nöje förenas för hela familjen. 
-                    Utforska en mångfald av livsformer, från majestätiska rovdjur till färgsprakande exotiska fåglar och lekfulla däggdjur.
-                </p>
-                <p>
-                    Stig in i en värld av spännande möten och lärorika stunder. 
-                    Våra erfarna guider står redo att dela med sig av intressanta fakta om våra fyrbenta vänner och deras naturliga miljöer. 
-                    Vi är stolta över att inte bara underhålla, utan också engagera våra besökare i bevarandet av vårt värdefulla djurliv.
-                </p>
-                <p>
-                    Utforska våra noggrant utformade utställningar som återskapar autentiska livsmiljöer. 
-                    Gå på safari genom savannen, utforska regnskogens mystik eller beundra korallrevets skönhet. 
-                    För de yngre upptäckarna finns interaktiva områden där de kan lära sig genom lek.
-                </p>
-                <p>
-                    Ditt besök hos oss stödjer också viktiga bevarandeprojekt och forskning som hjälper hotade arter och deras naturliga livsmiljöer. 
-                    Tillsammans kan vi göra en verklig skillnad.
-                </p>
-                <p>
-                    Upplev äventyret, närma dig naturen och skapa minnen för livet på vårt enastående zoo. 
-                    Köp dina biljetter idag och ge hela familjen en oförglömlig dag fylld med spännande upplevelser och djurupptäckter!
-                </p>
+                <h1>Välkommen till Nordiska Djurriket</h1>
+                <div className="flexContainer">
+                    <div>
+                        <p>
+                            Välkommen till vårt fantastiska zooäventyr! Upplev magin med en värld av fascinerande djur från hela jorden samlade på ett ställe. 
+                            Vår djurpark är en plats där lärande och nöje förenas för hela familjen. 
+                            Utforska en mångfald av livsformer, från majestätiska rovdjur till färgsprakande exotiska fåglar och lekfulla däggdjur.
+                        </p>
+                        <p>
+                            Stig in i en värld av spännande möten och lärorika stunder. 
+                            Våra erfarna guider står redo att dela med sig av intressanta fakta om våra fyrbenta vänner och deras naturliga miljöer. 
+                            Vi är stolta över att inte bara underhålla, utan också engagera våra besökare i bevarandet av vårt värdefulla djurliv.
+                        </p>
+                        <p>
+                            Utforska våra noggrant utformade utställningar som återskapar autentiska livsmiljöer. 
+                            Gå på safari genom savannen, utforska regnskogens mystik eller beundra korallrevets skönhet. 
+                            För de yngre upptäckarna finns interaktiva områden där de kan lära sig genom lek.
+                        </p>
+                        <p>
+                            Ditt besök hos oss stödjer också viktiga bevarandeprojekt och forskning som hjälper hotade arter och deras naturliga livsmiljöer. 
+                            Tillsammans kan vi göra en verklig skillnad.
+                        </p>
+                        <p>
+                            Upplev äventyret, närma dig naturen och skapa minnen för livet på vårt enastående zoo. 
+                            Köp dina biljetter idag och ge hela familjen en oförglömlig dag fylld med spännande upplevelser och djurupptäckter!
+                        </p>
+                    </div>
+                    <div>
+                        <img src={startpage} loading="lazy" alt="Deer in forest"></img>
+                    </div>
+                </div>
             </section>
         </>
     );
